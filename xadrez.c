12fc534d66c09cf25fa50d6e0d4be44603f912c6
Nilton Pontes <niltoneapontes@gmail.com>
2025-07-11 23:29:45 +0000

    feat: desafio nivel mestre

--- xadrez.c
+++ xadrez.c
@@ -25,48 +25,11 @@ void moverRainha(int casas) {
     }
 }
 
-int main() {
-    // Nível Novato - Movimentação das Peças
-    int i, bispo = 5, torre = 5, rainha = 8;
-    int cavaloHorizontal = 1, cavaloVertical = 2, opcao;
-    printf("### Bem vindo(a) ao jogo de Xadrez! ###\n");
-
-    // Implementação de Movimentação do Bispo
-    printf("\n");
-    printf("### Quantas casas o bispo deve andar na diagonal superior direita?\n");
-    scanf("%d", &bispo);
-    printf("\n");
-
-    moverBispo(bispo);
-
-    // Implementação de Movimentação da Torre
-    printf("\n");
-    printf("### Quantas casas a torre deve andar para a direita?\n");
-    scanf("%d", &torre);
-    printf("\n");
-
-    moverTorre(torre);
-
-    // Implementação de Movimentação da Rainha
-    printf("\n");
-    printf("### Quantas casas a rainha deve andar para a esquerda?\n");
-    scanf("%d", &rainha);
-    printf("\n");
-
-    moverRainha(rainha);
-
-    // Nível Aventureiro - Movimentação do Cavalo
-    printf("\n");
-    printf("### Selecione se o cavalo deve ir para a esquerda ou direita:\n");
-    printf("1. Esquerda\n");
-    printf("2. Direita\n");
-    scanf("%d", &opcao);
-    printf("\n");
-
-    for(int j = 1, k = 1; j <= cavaloHorizontal || k <= cavaloVertical; j++, k++) {
+void moverCavalo(int horizontal, int vertical, int opcao) {
+        for(int j = 1, k = 1; j <= horizontal || k <= vertical; j++, k++) {
         printf("Cima\n");
         
-        if(k == cavaloVertical) {
+        if(k == vertical) {
             switch(opcao) {
                 case 1:
                     printf("Esquerda\n");
@@ -80,6 +43,47 @@ int main() {
             } 
         }
     }
+}
+
+int main() {
+    // Nível Novato - Movimentação das Peças
+    int i, bispo = 5, torre = 5, rainha = 8;
+    int cavaloHorizontal = 1, cavaloVertical = 2, opcao;
+    printf("### Bem vindo(a) ao jogo de Xadrez! ###\n");
+
+    // Implementação de Movimentação do Bispo
+    printf("\n");
+    printf("### Quantas casas o bispo deve andar na diagonal superior direita?\n");
+    scanf("%d", &bispo);
+    printf("\n");
+
+    moverBispo(bispo); // Recursividade do Bispo
+
+    // Implementação de Movimentação da Torre
+    printf("\n");
+    printf("### Quantas casas a torre deve andar para a direita?\n");
+    scanf("%d", &torre);
+    printf("\n");
+
+    moverTorre(torre); // Recursividade da Torre
+
+    // Implementação de Movimentação da Rainha
+    printf("\n");
+    printf("### Quantas casas a rainha deve andar para a esquerda?\n");
+    scanf("%d", &rainha);
+    printf("\n");
+
+    moverRainha(rainha);  // Recursividade da Rainha
+
+    // Nível Aventureiro - Movimentação do Cavalo
+    printf("\n");
+    printf("### Selecione se o cavalo deve ir para a esquerda ou direita:\n");
+    printf("1. Esquerda\n");
+    printf("2. Direita\n");
+    scanf("%d", &opcao);
+    printf("\n");
+
+    moverCavalo(cavaloHorizontal, cavaloVertical, opcao); // Loops complexos para o Cavalo
 
     printf("\n\n");
 
